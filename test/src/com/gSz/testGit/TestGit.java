e40a84a49c98d346447bb1925dff28c04491f309
Gsz <489671337@qq.com>
2020-09-06 22:45:07 +0800

    first commit

---
+++ test/src/com/gSz/testGit/TestGit.java
@@ -0,0 +1,19 @@
+package com.gSz.testGit;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+
+}
